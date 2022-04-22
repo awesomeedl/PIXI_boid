c4237e446a6e8659939c02843dcb8a4e4eefb9c7
Edward <tliao512@uw.edu>
2022-04-21 22:40:25 -0700

    spawning2

--- main.js
+++ main.js
@@ -5,13 +5,12 @@ let height = document.body.clientHeight;
 let app = new PIXI.Application({ width, height });
 document.body.appendChild(app.view);
 
-let graphics = new PIXI.Graphics();
-app.stage.addChild(graphics);
-
 let number = 30;
 let boids = [];
 
 let texture = PIXI.Texture.from('Arrow.png');
+
+init();
 
 function init() {
     for(let i = 0; i < number; i++)
@@ -21,7 +20,7 @@ function init() {
 
         boid.x = Math.random() * width;
         boid.y = Math.random() * height;
-        
+
         app.stage.addChild(boid);
     }
 }
