fa586166a49083be0e4bdc6fe521a08e753f4b9a
awesomeedl <awesomeedl@users.noreply.github.com>
2023-02-17 03:22:18 +0000

    deploy: 8887dc69b8fc54a1991c43166af4bde5582a7c40

---
+++ main.mjs
@@ -0,0 +1,1 @@
+import{Application}from"pixi";import{Flock}from"./boid.mjs";let app=new Application({resizeTo:window,autoDensity:!0});document.body.appendChild(app.view);let flock=new Flock(500,app);app.ticker.add((()=>flock.run()));
